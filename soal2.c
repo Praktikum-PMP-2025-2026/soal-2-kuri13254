9eeb3cd14cc5398e2dbf06512a079de983522dd3
kuri13254 <riqquu@gmail.com>
2026-04-15 10:53:03 +0700

    solusi_soal2.c

--- soal2.c
+++ soal2.c
@@ -1,5 +1,31 @@
-#include <stdio.h>
+/*
+Soal Pilihan - Soal 2
+Nama    : Thoriq Al Maududi
+NIM     : 13224054
+*/
 
-int main(void) {
-    return 0;
+#include <stdio.h>
+/*Deklarasi Variabel*/
+int T;
+int n;
+int langkah;
+/*Deklarasi Fungsi*/
+int hitung(int n, int langkah){  
+    if(n==1) return langkah;
+    if(n%2 == 0){
+        n = n/2;
+    }
+    else{
+        n = 3*n + 1;
+    }
+    return hitung(n, langkah + 1);
+}
+
+/*Main*/
+int main(){
+    scanf("%d", &T);
+    for(int i = 0; i < T ; i++){
+        scanf("%d", &n);
+        printf("LANGKAH %d\n", hitung(n, 0));
+    }
 }
